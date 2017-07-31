08423f9458f762456489ffe965e81bd2a03e8eed
Mikko Peltonen <mikko@peltoset.fi>
2017-07-31 20:45:20 +0300

    dump

--- CameraSlider/CameraSlider.ino
+++ CameraSlider/CameraSlider.ino
@@ -1,3 +1,9 @@
+#include <AccelStepper.h>
+
+#define ENA 51
+#define PUL 47
+#define DIR 49
+
 enum Mode {
   Timelapse,
   Video,
@@ -13,12 +19,30 @@ int tilt = -5000;
 int focus = 0;
 int zoom = 5200;
 
+AccelStepper stepper(AccelStepper::DRIVER, PUL, DIR);
+
 void setup() {
   Serial2.begin(115200);
   Serial.begin(9600);
+
+  pinMode(ENA, OUTPUT);
+  pinMode(PUL, OUTPUT);
+  pinMode(DIR, OUTPUT);
+
+  stepper.setEnablePin(ENA);
+  stepper.setPinsInverted(false, false, true);
+  stepper.setMaxSpeed(2000);
+  stepper.setAcceleration(10000);
+  stepper.enableOutputs();
+
+  stepper.setCurrentPosition(0);
+  stepper.moveTo(43969);
+  stepper.setSpeed(500);
 }
 
 void loop() {
+  //stepper.move(1000);
+  
   if (Serial2.available()) {
     String msg = Serial2.readString();
     msg.trim();
@@ -37,6 +61,8 @@ void loop() {
   if (Serial.available()) {
     Serial2.write(Serial.read());
   }
+
+  stepper.run();
 }
 
 void goHome() {}
